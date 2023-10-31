0d81981e43692254e1446bb4def6c382b0dad0a5
Michael Bocim <mtbocim@gmail.com>
2023-10-30 20:10:53 -0600

    Absolute positioning is dumb

--- src/Frustrations.jsx
+++ src/Frustrations.jsx
@@ -31,43 +31,45 @@ function Frustrations() {
         if (frustrations.length !== 0) {
             setFireVisible(true);
             setTimeout(() => {
-                setFireVisible(false);
-                setFrustrations([])
-            }, 2000)
+                setFireVisible(()=>false);
+            }, 2250)
+            setFrustrations([])
         }
     }
 
     return (
         <div className="Frustrations">
+            {!fireVisible &&
+                <Form className="d-flex flex-column w-75">
+                    <Form.Text
+                        style={{ fontSize: "1.5em" }}
+                        className="d-flex justify-content-center flex-column align-items-center"
+                    >
+                        You currently have {frustrations.length} frustration{frustrations.length !== 1 && 's'} you could burn...
+                        <br />
+                        {/* <span className="d-flex justify-content-center align-content-md-around" style={{ fontSize: "0.5em", fontWeight: "bold" }}>
+                        (only in your mind at the moment!)
+                    </span> */}
+                    </Form.Text>
+                    <div className="mt-3 d-flex justify-content-center align-content-md-around">
+                        <InputGroup className="mb-3 w-50">
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter your frustrations here..."
+                                value={inputValue}
+                                onChange={handleChange}
+                            />
+                        </InputGroup>
+                    </div>
+                    <div className="Frustrations-button-container d-flex justify-content-center align-content-md-around">
+                        <Button variant="primary" onClick={handleSave}>Save</Button>
+                        <Button variant="warning" onClick={handleBurn}>Burn</Button>
+                    </div>
+                </Form>
+            }
             <div className={`Frustration-fire ${fireVisible ? 'fire-visible' : ''}`}>
                 <img className="Frustrations-fire-image" alt='fire' src={fire} />
             </div>
-            <Form className="d-flex flex-column w-75">
-                <Form.Text
-                    style={{ fontSize: "1.5em" }}
-                    className="d-flex justify-content-center flex-column align-items-center"
-                >
-                    You currently have {frustrations.length} frustration{frustrations.length !== 1 && 's'} you could burn...
-                    <br />
-                    {/* <span className="d-flex justify-content-center align-content-md-around" style={{ fontSize: "0.5em", fontWeight: "bold" }}>
-                        (only in your mind at the moment!)
-                    </span> */}
-                </Form.Text>
-                <div className="mt-3 d-flex justify-content-center align-content-md-around">
-                    <InputGroup className="mb-3 w-50">
-                        <Form.Control
-                            type="text"
-                            placeholder="Enter your frustrations here..."
-                            value={inputValue}
-                            onChange={handleChange}
-                        />
-                    </InputGroup>
-                </div>
-                <div className="Frustrations-button-container d-flex justify-content-center align-content-md-around">
-                    <Button variant="primary" onClick={handleSave}>Save</Button>
-                    <Button variant="warning" onClick={handleBurn}>Burn</Button>
-                </div>
-            </Form>
         </div>
     )
 
